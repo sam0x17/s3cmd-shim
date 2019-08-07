5c024b62874a9c4acb2a0dbc574833ccc12dffae
Sam Johnson <sam@durosoft.com>
2019-08-07 18:14:17 -0400

    connect with public docker

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker build -t s3cmd-docker . || exit 1
 mkdir -p ~/.s3cmd_docker || exit 1
 cp ./s3cmd ~/.s3cmd_docker/ || exit 1
 if grep -q "/.s3cmd_docker" "$HOME/.bashrc"; then
---
+++ push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push durosoft/s3cmd-shim:latest
