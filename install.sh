e19485a35b4e0d288a6fb6e5d3185250dcc3dc25
Sam Johnson <sam@durosoft.com>
2019-08-07 18:15:35 -0400

    use consistent naming

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p ~/.s3cmd_docker || exit 1
-cp ./s3cmd ~/.s3cmd_docker/ || exit 1
-if grep -q "/.s3cmd_docker" "$HOME/.bashrc"; then
+mkdir -p ~/.s3cmd_shim || exit 1
+cp ./s3cmd ~/.s3cmd_shim/ || exit 1
+if grep -q "/.s3cmd_shim" "$HOME/.bashrc"; then
   echo "s3cmd shim successfully updated"
   exit
 fi
-echo 'export PATH="$PATH:~/.s3cmd_docker/"' >> ~/.bashrc || exit 1
+echo 'export PATH="$PATH:~/.s3cmd_shim/"' >> ~/.bashrc || exit 1
 echo "s3cmd shim successfully installed"
