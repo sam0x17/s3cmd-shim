4a36c529cdac1368d791cd5b2168f30b7c3f98e3
Sam Johnson <sam@durosoft.com>
2019-08-07 21:41:30 -0400

    don't forget to chmod

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd ~/.s3cmd_shim || exit 1
 touch s3cmd || exit 1
 rm s3cmd || exit 1
 curl https://raw.githubusercontent.com/sam0x17/s3cmd-shim/master/s3cmd --output s3cmd || exit 1
+chmod +x s3cmd || exit 1
 docker pull durosoft/s3cmd-shim:latest || exit 1
 if grep -q "/.s3cmd_shim" "$HOME/.bashrc"; then
   echo "s3cmd shim successfully updated"
